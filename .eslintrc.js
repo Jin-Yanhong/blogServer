7b1c2105edd1efecf2773b329a636a83501c2fed
Jin-Yanhong <820877998@qq.com>
2022-10-19 18:19:30 +0800

    add code lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,22 @@
 module.exports = {
-	env: {
-		browser: true,
-		commonjs: true,
-		es2021: true,
-	},
-	extends: 'google',
-	overrides: [],
-	parserOptions: {
-		ecmaVersion: 'latest',
-	},
-	rules: {
-		indent: ['warn', 4],
-		quotes: ['warn', 'single'],
-		semi: ['warn', 'always'],
-		'max-len': ['warn', { code: 360 }],
-		'linebreak-style': ['warn', 'windows'],
-		'object-curly-spacing': ['warn', 'always'],
-		'space-before-function-paren': ['warn', 'always'],
-		'require-jsdoc': 0,
-		'valid-jsdoc': 0,
-	},
+    env: {
+        commonjs: true,
+        es2021: true,
+        node: true,
+    },
+    extends: 'eslint:recommended',
+    overrides: [],
+    parserOptions: {
+        ecmaVersion: 'latest',
+    },
+    rules: {
+        'indent': ['warn', 4],
+        'quotes': ['warn', 'single'],
+        'semi': ['warn', 'always'],
+        'max-len': ['warn', { code: 360 }],
+        'linebreak-style': ['warn', 'windows'],
+        'object-curly-spacing': ['warn', 'always'],
+        'require-jsdoc': 0,
+        'valid-jsdoc': 0,
+    },
 };
-
